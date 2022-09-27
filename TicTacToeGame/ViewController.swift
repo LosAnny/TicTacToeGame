3daa5101be70f35cde95738e063a2e7fa1d967dd
los.anny <ann.losh99@mail.ru>
2022-09-27 11:54:09 +0300

    Creating game logic.

--- TicTacToeGame/ViewController.swift
+++ TicTacToeGame/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Turn {
+        case nought
+        case cross
+    }
 
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    @IBOutlet weak var fieldA1: UIButton!
+    @IBOutlet weak var fieldA2: UIButton!
+    @IBOutlet weak var fieldA3: UIButton!
+    
+    @IBOutlet weak var fieldB1: UIButton!
+    @IBOutlet weak var fieldB2: UIButton!
+    @IBOutlet weak var fieldB3: UIButton!
+    
+    @IBOutlet weak var fieldC1: UIButton!
+    @IBOutlet weak var fieldC2: UIButton!
+    @IBOutlet weak var fieldC3: UIButton!
+    
+    var firstTurn = Turn.cross
+    var currentTurn = Turn.cross
+    
+    let nought = "O"
+    let cross = "X"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func boardTapAction(_ sender: UIButton) {
+        addToBoard(sender)
+    }
+    
+    func addToBoard(_ sender: UIButton) {
+        if sender.title(for: .normal) == nil {
+            if currentTurn == Turn.nought {
+                sender.setTitle(nought, for: .normal)
+                currentTurn = Turn.cross
+                turnLabel.text = cross
+            } else if currentTurn == Turn.cross {
+                sender.setTitle(cross, for: .normal)
+                currentTurn = Turn.nought
+                turnLabel.text = nought
+            }
+        }
+    }
 }
 
